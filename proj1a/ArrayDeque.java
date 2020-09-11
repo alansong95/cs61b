950122a944419dc2e1d634d45a292479fa468cfe
Alan Song <alansong95@gmail.com>
2020-09-11 08:21:02 -0700

    finish project 1a array

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,139 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] items;
+
+    public ArrayDeque() {
+        this.items = (T[]) new Object[8];
+        this.nextFirst = this.items.length / 2 - 1;
+        this.nextLast = this.items.length / 2;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    // resize needed
+    public void addFirst(T item) {
+        if (this.size == this.items.length) {
+            sizeUp();
+        }
+
+        this.items[nextFirst] = item;
+        this.nextFirst--;
+        this.size++;
+        if (this.nextFirst < 0) {
+            this.nextFirst = this.items.length - 1;
+        }
+    }
+
+    private void sizeUp() {
+        T[] temp = (T[]) new Object[this.items.length * 2];
+        int startIndex = temp.length / 4;
+        for (int i = nextLast; i < this.items.length; i++) {
+            temp[startIndex] = this.items[i];
+            startIndex++;
+        }
+
+        for (int i = 0; i < nextLast; i++) {
+            temp[startIndex] = this.items[i];
+            startIndex++;
+        }
+
+        this.items = temp;
+        this.nextFirst = temp.length / 4 - 1;
+        this.nextLast = temp.length / 4 * 3;
+    }
+
+    private void sizeDown() {
+        T[] temp = (T[]) new Object[this.items.length / 2];
+        int startIndex = temp.length / 4;
+
+        if (nextFirst < nextLast) {
+            for (int i = nextFirst + 1; i < nextLast; i++ ){
+                temp[startIndex] = this.items[i];
+                startIndex++;
+            }
+        } else{
+            for (int i = nextFirst + 1; i < this.items.length; i++) {
+                temp[startIndex] = this.items[i];
+                startIndex++;
+            }
+
+            for (int i = 0; i < nextLast; i++) {
+                temp[startIndex] = this.items[i];
+                startIndex++;
+            }
+        }
+        this.items = temp;
+        this.nextFirst = temp.length / 4 - 1;
+        this.nextLast = temp.length / 4 * 3;
+    }
+
+    public void addLast(T item) {
+        if (this.size == this.items.length) {
+            sizeUp();
+        }
+
+        this.items[nextLast] = item;
+        this.nextLast++;
+        this.size++;
+        if (this.nextLast >= this.items.length) {
+            this.nextLast = 0;
+        }
+    }
+
+    public void printDeque() {
+        if (nextFirst < nextLast) {
+            for (int i = nextFirst + 1; i < nextLast; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = nextFirst + 1; i < items.length; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            for (int i = 0; i < nextLast; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public T removeFirst() {
+        this.nextFirst++;
+        T toReturn = this.items[this.nextFirst];
+        this.size--;
+
+        if ((double) this.size / this.items.length < 0.25) {
+            sizeDown();
+        }
+        return toReturn;
+    }
+
+    public T removeLast() {
+        this.nextLast--;
+        T toReturn = this.items[this.nextLast];
+        size--;
+
+        if ((double) this.size / this.items.length < 0.25) {
+            sizeDown();
+        }
+        return toReturn;
+    }
+
+    public T get(int index) {
+        if (nextFirst < nextLast) {
+            return this.items[nextFirst + 1 + index];
+        } else {
+            int temp = nextFirst + 1 + index - this.items.length;
+            return this.items[temp];
+        }
+    }
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,162 @@
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+/** Performs some basic linked list tests. */
+public class ArrayDequeTest {
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkGet(String expected, String actual) {
+        if (expected != actual) {
+            System.out.println("get() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Prints a nice message based on whether a test passed.
+     * The \n means newline. */
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    /** Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     *
+     * && is the "and" operation. */
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        ArrayDeque<String> lld1 = new ArrayDeque<String>();
+
+        boolean passed = checkEmpty(true, lld1.isEmpty());
+
+        lld1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        passed = checkSize(1, lld1.size()) && passed;
+        passed = checkEmpty(false, lld1.isEmpty()) && passed;
+
+        lld1.addLast("middle");
+        passed = checkSize(2, lld1.size()) && passed;
+
+        lld1.addLast("back");
+        passed = checkSize(3, lld1.size()) && passed;
+
+        System.out.println("Printing out deque: ");
+        lld1.printDeque();
+
+        printTestStatus(passed);
+    }
+
+    /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
+    public static void addRemoveTest() {
+
+        System.out.println("Running add/remove test.");
+
+        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
+        // should be empty
+        boolean passed = checkEmpty(true, lld1.isEmpty());
+
+        lld1.addFirst(10);
+        // should not be empty
+        passed = checkEmpty(false, lld1.isEmpty()) && passed;
+
+        lld1.removeFirst();
+        // should be empty
+        passed = checkEmpty(true, lld1.isEmpty()) && passed;
+
+        printTestStatus(passed);
+    }
+
+    public static void addGetTest() {
+        System.out.println("Running add/get test.");
+        ArrayDeque<String> lld1 = new ArrayDeque<String>();
+
+        lld1.addFirst("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+
+
+
+        boolean passed = checkGet("front", lld1.get(0));
+        passed = checkGet("middle", lld1.get(1)) && passed;
+        passed = checkGet("back", lld1.get(2)) && passed;
+        passed = checkGet(null, lld1.get(3)) && passed;
+        printTestStatus(passed);
+    }
+
+    public static void manyAddsTest() {
+        System.out.println("Running many adds test.");
+        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
+
+//        for (int i = 0; i < 3; i++) {
+//            lld1.addFirst(i);
+//        }
+        for (int i = 0; i < 1000; i++) {
+            lld1.addLast(i);
+        }
+        lld1.printDeque();
+
+
+    }
+
+    public static void manyAddsDeletesTest() {
+        System.out.println("Running many adds/deletes test.");
+        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
+
+        for (int i = 500; i >= 0; i--) {
+            lld1.addFirst(i);
+        }
+        for (int i = 501; i <= 1000; i++) {
+            lld1.addLast(i);
+        }
+
+        lld1.printDeque();
+
+        for (int i = 0; i < 900; i++) {
+            lld1.removeFirst();
+        }
+
+        lld1.printDeque();
+        lld1.size();
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+//        addIsEmptySizeTest();
+//        addRemoveTest();
+//        addGetTest();
+
+//        manyAddsTest();
+        manyAddsDeletesTest();
+    }
+}
